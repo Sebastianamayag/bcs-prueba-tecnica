aa1721e156acdf5f26a12b19a4ba6d4c33adc126
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2026-04-25 10:25:18 -0500

    feat: add personalized fetch

---
+++ shared/fetch/Fetch.ts
@@ -0,0 +1,19 @@
+export const  Fetch = async <T>(url: string, options?: RequestInit): Promise<T> => {
+  const res = await fetch(url, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      "auth": ''
+    },
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    throw {
+      message: data?.message || "Error",
+      status: res.status,
+      data,
+    };
+  }
+
+  return data;
+}
