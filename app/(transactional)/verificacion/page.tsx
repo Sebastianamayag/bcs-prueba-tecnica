a1f52cee47314984f936ccaf42b4cd2caa888c06
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2026-04-26 08:10:25 -0500

    fix: change export

--- app/(transactional)/verificacion/page.tsx
+++ app/(transactional)/verificacion/page.tsx
@@ -1,10 +1,9 @@
 import { Card } from "@/shared/components/Card/Card";
-import { Stepper } from "@/shared/components/Stepper/Stepper";
 
 export default function SimulationPage() {
   return (
     <Card>
-      <Stepper latsStepComppleted={3} />
+      <p>hola</p>
     </Card>
   )
 }
