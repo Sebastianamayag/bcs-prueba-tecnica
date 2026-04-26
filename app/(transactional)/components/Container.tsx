43767a1304f05bf6b4d732d62b7a419657b98a71
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2026-04-26 09:56:44 -0500

    fix: import

--- app/(transactional)/components/Container.tsx
+++ app/(transactional)/components/Container.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { Stepper } from '@/shared/components/Stepper/Stepp';
 import { ReactNode } from 'react';
 import { useAppStore } from '../store/client';
+import { Stepper } from '@/shared/components/Stepper/Stepp';
 
 export const Container = ({ children }: { children: ReactNode }) => {
   const latsStepComppleted = useAppStore((state) => state.latsStepComppleted);
