0136647999dbeea0327417d887ff4bcd8435253c
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2026-04-26 12:31:05 -0500

    feat: create financial data form

--- app/(transactional)/datos-personales/components/BasicDataForm.tsx
+++ app/(transactional)/datos-personales/components/BasicDataForm.tsx
@@ -7,19 +7,20 @@ import { InputForm } from "./InputForm";
 export const BasicDataForm = () => {
   const {control} = useFormContext();
   return (
-    <div className="px-20">
+    <div className="px-20 mt-10">
         <Card>
+          <h2 className="font-bold w-3/4 mb-15 " data-testid='datos_basicos'>Datos Basicos</h2>
           <div className="space-y-5">
             <div className="flex flex-col md:flex-row gap-2 flex-1">
-              <InputForm control={control} label={"Número de documento"} id={"num_doc"} name={"numero_documento"} />
-              <InputForm control={control} label={"Tipo"} id={"tipo"} name={"tipo_de_documento"} />
+              <InputForm control={control} label={"Número de documento *"} id={"num_doc"} name={"numero_documento"} />
+              <InputForm control={control} label={"Tipo *"} id={"tipo"} name={"tipo_de_documento"} />
             </div>
             <div className="flex flex-col md:flex-row gap-2  flex-1">
-              <InputForm control={control} label={"Primer Nombre"} id={"primer_nombre"} name={"primer_nombre"} />
+              <InputForm control={control} label={"Primer Nombre *"} id={"primer_nombre"} name={"primer_nombre"} />
               <InputForm control={control} label={"Segundo Nombre"} id={"segundo_nombre"} name={"segundo_nombre"} />
             </div>
             <div className="flex flex-col md:flex-row gap-2  flex-1">
-              <InputForm control={control} label={"Primer Apellido"} id={"primer_apellido"} name={"primer_apellido"} />
+              <InputForm control={control} label={"Primer Apellido *"} id={"primer_apellido"} name={"primer_apellido"} />
               <InputForm control={control} label={"Segundo Apellido"} id={"segundo_apellido"} name={"segundo_apellido"} />
             </div>
           </div>
