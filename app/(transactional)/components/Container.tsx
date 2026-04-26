ef3c2a7362af3725be5773439619df5fd333adc7
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2026-04-26 09:58:15 -0500

    fix: import relative path

--- app/(transactional)/components/Container.tsx
+++ app/(transactional)/components/Container.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { ReactNode } from 'react';
 import { useAppStore } from '../store/client';
-import { Stepper } from '@/shared/components/Stepper/Stepp';
+import { Stepper } from '../../../shared/components/Stepper/Stepp';
 
 export const Container = ({ children }: { children: ReactNode }) => {
   const latsStepComppleted = useAppStore((state) => state.latsStepComppleted);
