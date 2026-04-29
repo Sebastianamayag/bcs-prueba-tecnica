457a906d6e2097379c3a3113b19afc858e9ebf90
Sebastianamayag <sebasamaya.gomez@gmail.com>
2026-04-29 16:13:21 -0500

    fix: end points

--- app/(transactional)/datos-personales/hooks/useSaveDataClient.ts
+++ app/(transactional)/datos-personales/hooks/useSaveDataClient.ts
@@ -5,69 +5,76 @@ import { Fetch } from "@/shared/fetch/Fetch";
 import { useRouter } from "next/navigation";
 
 export const useSaveDataClient = () => {
-    const { formState, trigger, watch } = useFormContext();
-    const { isValid } = formState;
-    const values = watch();
+  const { formState, trigger, watch } = useFormContext();
+  const { isValid } = formState;
+  const values = watch();
 
-    const { setDatosBasicos, setDatosFinancieros, setTerminos, setLastStep, numero_afiliacion } = useAppStore();
-    const { setIsLoading, setToastMessage } = useGlobalUI();
-    const router = useRouter();
-    const parseData = () => {
-        return {
-            datosBasicos: {
-                numero_documento: values.numero_documento,
-                tipo_de_documento: values.tipo_de_documento,
-                primer_nombre: values.primer_nombre,
-                segundo_nombre: values.segundo_nombre,
-                primer_apellido: values.primer_apellido,
-                segundo_apellido: values.segundo_apellido,
-            },
-            datosFinancieros: {
-                cargo: values.cargo,
-                ingresos: values.ingresos,
-                egresos: values.egresos,
-                patrimonio: values.patrimonio,
-            },
-            terminos: values.terminos
-        }
-    }
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!isValid) return await trigger();
-        try {
-            setIsLoading(true);
-            const { datosBasicos, datosFinancieros, terminos } = parseData();
-            setDatosBasicos(datosBasicos);
-            setDatosFinancieros(datosFinancieros);
-            setTerminos(terminos);
-            await Fetch(
-                `/api/aplications/${numero_afiliacion}`,
-                process.env.NEXT_PUBLIC_API_PATCH_APPLICATIONS ?? '',
-                undefined,
-                {
-                    method: 'PATCH',
-                    body: JSON.stringify({
-                        numero_afiliacion: numero_afiliacion,
-                        data: {
-                            datosBasicos,
-                            datosFinancieros,
-                            terminos,
-                        }
-                    }),
-                }
-            );
-            setLastStep(2);
-            router.push('/verificacion');
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (error: any) {
-            setToastMessage(error.message, 'error')
-        } finally {
-            setIsLoading(false);
-        }
-    }
-
+  const {
+    setDatosBasicos,
+    setDatosFinancieros,
+    setTerminos,
+    setLastStep,
+    numero_afiliacion,
+  } = useAppStore();
+  const { setIsLoading, setToastMessage } = useGlobalUI();
+  const router = useRouter();
+  const parseData = () => {
     return {
-        handleSubmit,
+      datosBasicos: {
+        numero_documento: values.numero_documento,
+        tipo_de_documento: values.tipo_de_documento,
+        primer_nombre: values.primer_nombre,
+        segundo_nombre: values.segundo_nombre,
+        primer_apellido: values.primer_apellido,
+        segundo_apellido: values.segundo_apellido,
+      },
+      datosFinancieros: {
+        cargo: values.cargo,
+        ingresos: values.ingresos,
+        egresos: values.egresos,
+        patrimonio: values.patrimonio,
+      },
+      terminos: values.terminos,
+    };
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!isValid) return await trigger();
+    try {
+      setIsLoading(true);
+      const { datosBasicos, datosFinancieros, terminos } = parseData();
+      setDatosBasicos(datosBasicos);
+      setDatosFinancieros(datosFinancieros);
+      setTerminos(terminos);
+      await Fetch(
+        `/api/aplications/${numero_afiliacion}`,
+        undefined,
+        undefined,
+        {
+          method: "PATCH",
+          body: JSON.stringify({
+            numero_afiliacion,
+            data: {
+              datosBasicos,
+              datosFinancieros,
+              terminos,
+            },
+            scenario: process.env.NEXT_PUBLIC_API_PATCH_APPLICATIONS,
+          }),
+        },
+      );
+      setLastStep(2);
+      router.push("/verificacion");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      setToastMessage(error.message, "error");
+    } finally {
+      setIsLoading(false);
     }
-}
+  };
+
+  return {
+    handleSubmit,
+  };
+};
